ad8207db826834d447dba015fff8bd2eaed95aed
François Martineau <franccy29@gmail.com>
2021-11-15 02:11:29 +0000

    can create a new category now

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   resources :about_us, only: [:index]
+ 
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :create, :new]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
